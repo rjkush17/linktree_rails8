e9c800091a9eb86bc564ce983d524232dafb93bd
rjkush17 <rjkush17rishabh@gmail.com>
2025-01-06 08:03:21 +0530

    new controller created

---
+++ app/controllers/tree_controller.rb
@@ -0,0 +1,50 @@
+class TreeController < ApplicationController
+  before action :set_tree, only: %i[show edit update destroy]
+
+  def index
+    @trees = Tree.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+
+  def create
+    @tree = Tree.new(tree_params)
+    if @tree.save
+      redirect_to @tree, notice: "Tree created succesfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @tree.update(tree_params)
+        redirect_to @tree, notice: "tree was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tree.destroy
+    redirect_to trees_url, notice: "Tree was successfully destroyed."
+  end
+
+
+  private
+
+  def set_tree
+    @tree = Tree.find(params[:id])
+  end
+
+  def tree_params
+    params.require(:tree).permit(:name, :facebook, :x, :instagram, :website, :user_id)
+  end
+end
